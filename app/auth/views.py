b4014ecdec3eaa1b42a3422c15e65b07b001b6f8
bbcyyb <bbcyyb@gmail.com>
2018-02-11 18:00:55 +0800

    Implement auth logic

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,19 +1,92 @@
 # -*- coding:utf-8 -*-
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required, current_user
-from . import auth
+from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask_login import current_user
+from . import auth as app
+from .forms import LoginForm
+from .froms import RegisterForm
+from .. import db
+from ..model.user import User
+from ..common.email import send_mail
 
 
-@auth.route("/auth/login", methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<h1>login</h1>'
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('blog.index'))
+        flash(u'帐号或者密码错误')
+    return render_template('login.html', title=u'登陆', form=form)
 
 
-@auth.route('/auth/logout', methods=['GET'])
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
 def logout():
-    return '<h1>logout</h1>'
+    logout_user()
+    flash(u'您已退出登陆')
+    return redirect(url_for('auth.login'))
 
 
-@auth.route('/auth/register', methods=['GET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return '<h1>register</h1>'
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    password=form.password.data, email=form.email.data)
+        db.session.add(user)
+        db.session.commit()
+        User.add_self_follows()
+        token = user.generate_confirm_token()
+        send_mail(user.email, u'请确认您的帐号', 'confirm',
+                  user=user, token=token)
+        flash(u'有一份邮件已经发往您的邮箱')
+        return redirect(url_for('auth.login'))
+    else:
+        return render_template('register.html', title=u'注册', form=form)
+
+
+@app.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash(u'感谢您的确认')
+    else:
+        flash(u'链接已经失效或者过期')
+    return redirect(url_for('main.index'))
+
+
+@app.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint[:5] != 'auth.':
+            return redirect(url_for('auth.unconfirmed'))
+
+
+@app.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('unconfirmed.html')
+
+
+@app.route('/resend_email')
+@login_required
+def resend_email():
+    token = current_user.generate_confirm_token()
+    send_mail(current_user.email, u'确认您的帐号', 'confirm',
+              user=current_user, token=token)
+    flash(u'一份新的邮件已经发往您的邮箱')
+    return redirect(url_for('main.index'))
